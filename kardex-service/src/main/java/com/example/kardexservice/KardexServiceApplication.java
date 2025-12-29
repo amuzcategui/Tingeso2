212811b88e8fb8c77dac46182fd2cce239ce460b
amuzcategui <ambar.uzcategui@usach.cl>
2025-12-28 23:03:10 -0300

    los yaml

---
+++ kardex-service/src/main/java/com/example/kardexservice/KardexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kardexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardexServiceApplication.class, args);
+	}
+
+}
---
+++ kardex-service/src/test/java/com/example/kardexservice/KardexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kardexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
